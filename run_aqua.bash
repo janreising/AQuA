d6072026d038ce00b167d397078b2a3508d1d343
janreising <jan.reising@ki.se>
2021-04-01 11:00:56 +0200

    reverted changes

--- run_aqua.bash
+++ run_aqua.bash
@@ -6,6 +6,20 @@
 ##SBATCH --mem 768G
 #SBATCH -J janrei_aqua
 
+while getopts p:f: flag
+do
+	case "${flag}" in
+		p) preset=${OPTARG};;
+		f) file=${OPTARG};;
+	esac
+done
+
+# check if flags are set
+if [ -z ${preset+x} ]; then echo "Please set a preset!"; exit 1; else echo "Processing with preset $preset"; fi 
+#if [ -z ${folder+x} ]; then echo "Please set a folder!"; exit 1; else echo "Processing in folder $folder"; fi 
+if [ -z ${file+x} ]; then echo "Please set a file!"; exit 1; else echo "Processing of file $file"; fi 
+
+
 input = $1
 echo "Analyzing: $input"
 
@@ -22,4 +36,4 @@ cd ~/Private/aqua_janrei/
 # run script
 #srun matlab -r "preset=$preset;file='$file';try, run('aqua_cmd_dynamic.m'); end; quit"
 #srun matlab -r "try, run('aqua_cmd.m'); end; quit"
-srun matlab -r "preset='total_channels='$totalchannels';$preset';channel='$firstframe';input='$input';run('aqua_cmd.m');"
+srun matlab -r "preset='$preset';input='$file';run('aqua_cmd.m');"
