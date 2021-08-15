9b1147197d3aa2e1ee28146d9073c726aefeb089
jan.reising@ki.se <jan.reising@ki.se>
2021-08-15 10:24:46 +0200

    small bug fix

--- keb_cmd.m
+++ keb_cmd.m
@@ -4,8 +4,8 @@ startup;  % initialize
 load('random_Seed');
 rng(s);
 
-%preset = 2;
-%file = "/home/carmichael/Downloads/2-40X-loc1.cnmfe.h5";
+preset = 2;
+file = "/media/carmichael/LaCie SSD/JR/data/ca_imaging/15.07.21/slice2/2-40X-loc1.dwn.final.h5";
 
 %% save path
 [folder, name, ext] = fileparts(file);
