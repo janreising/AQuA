6d081a4a271d81e2c91ae676cd2800c93493f0a4
Jan Philipp Reising <jan.reising@ki.se>
2022-06-27 18:06:31 +0200

    chunked distributions works

--- actTop_noPackage.py
+++ actTop_noPackage.py
@@ -1,5 +1,7 @@
 import argparse
 import os
+import random
+
 import h5py as h5
 import tiledb
 import numpy as np
@@ -64,11 +66,11 @@ def run(meta, file_path, dataset=None, threshold=3, min_size=20, moving_average=
 
         print("Saving event map to: ", event_map_path)
         if output_folder is not None:
-            da.to_npy_stack(event_map_path, event_map, axis=0)
+            event_map.to_tiledb(event_map_path)
 
     else:
         print("Loading event map from: ", event_map_path)
-        event_map = da.from_npy_stack(event_map_path)
+        event_map = da.from_tiledb(event_map_path)
 
     # getting rid of extra variables
     print("Collecting garbage")
@@ -76,15 +78,26 @@ def run(meta, file_path, dataset=None, threshold=3, min_size=20, moving_average=
     gc.collect()
     time.sleep(2)
 
-    print("Calculating features")
-    return event_map
+    print("Calculating time map")
+    time_map_path = f"{output_folder}time_map.npy"
+    if not os.path.isfile(time_map_path):
+        time_map = get_time_map(event_map.compute())
 
-    events = custom_slim_features(event_map, file_path)
+        print("Saving event map to: ", time_map_path)
+        if output_folder is not None:
+            np.save(time_map_path, time_map)
+    else:
+        print("Loading time map from: ", time_map_path)
+        time_map = np.load(time_map_path)
+
+    del event_map
+    gc.collect()
+
+    print("Calculating features")
+    custom_slim_features(time_map, file_path, event_map_path)
 
     print("Saving")
     if output_folder is not None:
-        np.save(f"{output_folder}/events.npy", events)
-
         with open(f"{output_folder}/meta.json", 'w') as outfile:
             json.dump(meta, outfile)
 
@@ -211,45 +224,75 @@ def get_events(data: np.array, roi_threshold: float, var_estimate: float,
     return event_map
 
 
-def custom_slim_features(event_map, file_path):
+def custom_slim_features(time_map, data_path, event_path):
 
-        print(event_map)
-        sh_em = shared_memory.SharedMemory(create=True, size=event_map.nbytes)
-        shn_em = np.ndarray(event_map.shape, dtype=event_map.dtype, buffer=sh_em.buf)
-        shn_em[:] = event_map
+        # print(event_map)
+        # sh_em = shared_memory.SharedMemory(create=True, size=event_map.nbytes)
+        # shn_em = np.ndarray(event_map.shape, dtype=event_map.dtype, buffer=sh_em.buf)
+        # shn_em[:] = event_map
+        #
+        # num_events = np.max(shn_em)
 
-        num_events = np.max(shn_em)
+        # create chunks
+        print("\t collecting arguments")
+        num_frames, num_rois = time_map.shape
+
+        split_size = int(num_frames / (mp.cpu_count() * 10))
+        splits = np.arange(0, num_frames, split_size)
+
+        last_max_roi = 0
+        c = []
+        for split in splits:
+
+            start, stop = split, split+split_size
+
+            if stop >= num_frames:
+                c.append([start, num_frames, last_max_roi, num_rois, len(splits)])
+                continue
+
+            last_frame = np.where(time_map[stop, :] == 1)[0]
+            max_roi = np.max(last_frame)
+
+            zs, _ = np.where(time_map[:, last_frame[1:]] == 1)
+            max_z = np.max(zs)
+
+            c.append([start, max_z, last_max_roi+1, max_roi, len(splits)])
+            last_max_roi = max_roi
+
+        random.shuffle(c)
+        print("Num chunks: {} [{}]".format(len(c), len(c[0])))
+
+        out_path = os.sep.join(event_path[:-1].split(os.sep)[:-1]) + "/events/"
+        if not os.path.isdir(out_path):
+            os.mkdir(out_path)
+
+        arguments = zip(c, repeat(data_path), repeat(event_path), repeat(out_path))
 
         with mp.Pool(mp.cpu_count()) as p:
 
-            # TODO provide chunks; less overhead loading
-            print("\t collecting arguments ...")
-            arguments = zip(range(1, num_events), repeat(event_map.shape), repeat(sh_em.name), repeat(file_path))
-
             print("\t starting mapping")
-            R = p.starmap(func, arguments)
+            R = p.starmap(func_multi, arguments)
 
-        sh_em.close()
-        sh_em.unlink()
+        # sh_em.close()
+        # sh_em.unlink()
 
         return R
 
 
 def func(event_id, shape, sh_event_name, file_path):
+
     from multiprocess import shared_memory
     import numpy as np
     import tiledb
 
-    res = {}
-    res["label"] = event_id
-
+    # load data
     em = shared_memory.SharedMemory(name=sh_event_name)
     em_np = np.ndarray(shape, dtype='uint16', buffer=em.buf)
 
-    # d = shared_memory.SharedMemory(name=sh_data_name)
-    # d_np = np.ndarray(shape, dtype='float32', buffer=d.buf)
-
     data = tiledb.open(file_path)
+
+    res = {}
+    res["label"] = event_id
 
     z, x, y = np.where(em_np == event_id)
     z0, z1 = np.min(z), np.max(z)
@@ -283,6 +326,63 @@ def func(event_id, shape, sh_event_name, file_path):
 
     return res
 
+def func_multi(task, data_path, event_path, out_path):
+
+    import numpy as np
+    import tiledb
+
+    t0, t1, r0, r1, task_num = task
+    res_path = f"{out_path}events{r0}-{r1}.npy"
+
+    if os.path.isfile(res_path):
+        return 2
+
+    # load data
+    data = tiledb.open(data_path)[t0:t1]
+    event_map = tiledb.open(event_path)[t0:t1]
+
+    res = {}
+    for event_id in range(r0, r1):
+
+        res[event_id] = {}
+
+        try:
+            z, x, y = np.where(event_map == event_id)
+            z0, z1 = np.min(z), np.max(z)
+            x0, x1 = np.min(x), np.max(x)
+            y0, y1 = np.min(y), np.max(y)
+
+            res[event_id]["area"] = len(z)
+            res[event_id]["bbox"] = ((z0, z1), (x0, x1), (y0, y1))
+
+            dz, dx, dy = z1 - z0, x1 - x0, y1 - y0
+            z, x, y = z - z0, x - x0, y - y0
+            res[event_id]["dim"] = (dz + 1, dx + 1, dy + 1)
+            res[event_id]["pix_num"] = int((dz+1)*(dx+1)*(dy+1))
+
+            mask = np.ones((dz + 1, dx + 1, dy + 1), dtype=np.bool8)
+            mask[(z, x, y)] = 0
+            res[event_id]["mask"] = mask.flatten()
+            res[event_id]["footprint"] = np.invert(np.min(mask, axis=0)).flatten()
+
+            signal = data[z0:z1 + 1, x0:x1 + 1, y0:y1 + 1]  # TODO weird that i need +1 here
+            msignal = np.ma.masked_array(signal, mask)
+            res[event_id]["trace"] = np.ma.filled(np.nanmean(msignal, axis=(1, 2)))
+
+            res[event_id]["error"] = 0
+
+        except ValueError as err:
+            print("\t Error in ", event_id)
+            print("\t", err)
+            res[event_id]["error"] = 1
+            return -1
+
+    np.save(res_path, res)
+
+    num_files = len(os.listdir(out_path))
+    print("Finished task {}-{} ({:.1f}%)".format(t0, t1, num_files/task_num*100))
+
+    return 1
 
 def get_time_map(event_map):
 
@@ -321,12 +421,16 @@ if __name__ == "__main__":
     dt = time.time() - t0
     print("{:.1f} min".format(dt / 60) if dt > 60 else "{:.1f} s".format(dt))
 
+    sys.exit(2)
+
+    event_map_path = "/home/janrei1@ad.cmm.se/Desktop/22A5x4-2.subtr.reconstr.res/event_map/"
+    res = func_multi((1, 100, 1, 45), path, event_map_path)
 
     # split tm
-    tm = get_time_map(em)
+    time_map = get_time_map(em)
 
 
-    num_frames, num_rois = tm.shape
+    num_frames, num_rois = time_map.shape
 
     split_size = int(num_frames / (mp.cpu_count() * 25))
     splits = np.arange(0, num_frames, split_size)
@@ -341,10 +445,10 @@ if __name__ == "__main__":
             c.append([start, num_frames, last_max_roi, num_rois])
             continue
 
-        last_frame = np.where(tm[stop, :] == 1)[0]
+        last_frame = np.where(time_map[stop, :] == 1)[0]
         max_roi = np.max(last_frame)
 
-        zs, _ = np.where(tm[:, last_frame[1:]] == 1)
+        zs, _ = np.where(time_map[:, last_frame[1:]] == 1)
         max_z = np.max(zs)
 
         stop = max_z
